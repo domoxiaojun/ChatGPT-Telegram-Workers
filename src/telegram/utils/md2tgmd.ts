a8987ff89971ed9961412efac1633014878bbf16
adolphnov <adolphzhang@live.com>
2025-01-18 23:21:46 +0800

    fix: simplify & fix code chunk message logic

--- src/telegram/utils/md2tgmd.ts
+++ src/telegram/utils/md2tgmd.ts
@@ -167,18 +167,24 @@ export function chunkDocument(text: string, chunkSize: number = 4000): string[] 
             chunkIndex++;
             chunks.push([]);
             if (codeStack.length > 0) {
-                // 如果插入结尾标记后超出长度限制
-                if (chunks[chunkIndex - 1].join('\n').length + codeStack.length * 4 >= chunkSize) {
-                    // 将上一个块中的末尾数据插入到新块开头
-                    chunks[chunkIndex].push(...chunks[chunkIndex - 1].slice(-codeStack.length));
-                    // 将上一个块中的末尾行取出
-                    chunks[chunkIndex - 1].length -= codeStack.length;
-                } else {
-                    // 插入结尾标记
-                    chunks[chunkIndex - 1].push(...Array.from({ length: codeStack.length }).fill('```') as string[]);
+                // 存在末尾行为代码块起始导致分块异常，已存在冗余长度故不在处理
+                // // 如果插入结尾标记后超出长度限制
+                // if (chunks[chunkIndex - 1].join('\n').length + codeStack.length * 4 >= chunkSize) {
+                //     // 将上一个块中的末尾数据插入到新块开头
+                //     chunks[chunkIndex].push(...chunks[chunkIndex - 1].slice(-codeStack.length));
+                //     // 将上一个块中的末尾行取出
+                //     chunks[chunkIndex - 1].length -= codeStack.length;
+                // }
+                // 插入结尾标记
+                chunks[chunkIndex - 1].push(...Array.from({ length: codeStack.length }).fill('```') as string[]);
+                if (line.trim() === '```') {
+                    codeStack.pop();
                     // 插入开头标记
                     chunks[chunkIndex].unshift(...codeStack);
+                    continue;
                 }
+                // 插入开头标记
+                chunks[chunkIndex].unshift(...codeStack);
                 // 存在冗余, 不考虑以下情况: 新块代码行加line超出限制
                 // if (chunks[chunkIndex].join('\n').length + line.length > chunkSize) {
                 // // 插入结尾标记
