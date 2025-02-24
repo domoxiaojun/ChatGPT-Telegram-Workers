cf2e6195caf17bfc3b30b9697ed71f41b98f507e
adolphnov <adolphzhang@live.com>
2025-02-24 23:56:24 +0800

    fix: trigger message is not replaced

--- src/telegram/handler/group.ts
+++ src/telegram/handler/group.ts
@@ -67,6 +67,7 @@ export function CheckTrigger(message: Telegram.Message): boolean {
 
     const textBefore = message.text || message.caption || '';
     const text = textBefore.replace(new RegExp(`^${ENV.CHAT_TRIGGER_PERFIX}`), '').trim();
+    message.text ? message.text = text : message.caption = text;
     return text !== textBefore;
 }
 
