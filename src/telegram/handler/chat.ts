f24f40516768797fd7645ecebf6154d3af8eef0b
SzeMeng76 <80573337+SzeMeng76@users.noreply.github.com>
2025-05-17 22:15:35 +0800

    Update chat.ts

--- src/telegram/handler/chat.ts
+++ src/telegram/handler/chat.ts
@@ -350,9 +350,9 @@ export function OnStreamHander(sender: MessageSender | ChosenInlineSender, conte
         log.info('--- start end ---');
         streamSender.clearHeartbeat();
         await sentPromise;
-        if (nextEnableTime !== null && nextEnableTime > Date.now()) {
-            log.info(`Need await: ${nextEnableTime - Date.now()}ms`);
-            await waitUntil(nextEnableTime + 10);  
+        if ((nextEnableTime || 0) > Date.now()) {
+            log.info(`Need await: ${(nextEnableTime || 0) - Date.now()}ms`);
+            await waitUntil(nextEnableTime! + 10);
         }
         if (type === 'error') {
             text = `${cache}\n${text}`;
