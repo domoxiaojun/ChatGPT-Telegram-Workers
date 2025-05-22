a726fdf0ad91297150d1ed7d5562bfcbb2db9da5
adolphnov <adolphzhang@live.com>
2025-05-22 22:35:00 +0800

    feat: add vars: `BLOCK_COMMANDS`, `BLOCK_TOOLS`, `BLOCK_AGENTS` to disable unnecessary modules.

--- src/config/config.ts
+++ src/config/config.ts
@@ -72,10 +72,16 @@ export class EnvironmentConfig {
     //
     // 隐藏部分命令按钮
     HIDE_COMMAND_BUTTONS: string[] = [];
+    // 禁用部分命令
+    BLOCK_COMMANDS: string[] = [];
     // 显示快捷回复按钮
     SHOW_REPLY_BUTTON = false;
     // 额外引用消息开关
     EXTRA_MESSAGE_CONTEXT = false;
+    // 禁用内置工具
+    BLOCK_TOOLS: string[] = [];
+    // 禁用Agent
+    BLOCK_AGENTS: string[] = [];
 
     // -------------
 
