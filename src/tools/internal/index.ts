90d53f8887f7624ef285874f578979e32d9b09d6
SzeMeng76 <enquiry@ikmfoundation.org>
2025-12-19 15:11:30 +0800

    remove: delete image_edit tool

--- src/tools/internal/index.ts
+++ src/tools/internal/index.ts
@@ -3,7 +3,6 @@ import duckduckgo from './duckduckgo';
 import google_buildin from './google_buildin';
 import google_veo from './google_veo';
 import image_gen from './image_gen';
-import image_edit from './image_edit';
 import xiaohongshu from './xiaohongshu';
 
 export { default as tasks } from './scheduletask';
@@ -11,7 +10,6 @@ export { default as tasks } from './scheduletask';
 export default {
     duckduckgo,
     image_gen,
-    image_edit,
     xiaohongshu,
     app_iap,
     google_veo,
